50248d00a24b8a920d857ea6940d4f589168912f
Yoel Monzon <yoelfme@hotmail.com>
2015-05-19 23:15:50 -0600

    [add] routes and controller for tickets

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,16 +4,32 @@
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', [
+    'as' => 'tickets.latest',
+    'uses' => 'TicketsController@latest'
+]);
 
-Route::get('home', 'HomeController@index');
+Route::get('/populares', [
+    'as' => 'tickets.popular',
+    'uses' => 'TicketsController@popular'
+]);
+
+Route::get('/pendientes', [
+    'as' => 'tickets.open',
+    'uses' => 'TicketsController@open'
+]);
+
+Route::get('/tutoriales', [
+    'as' => 'tickets.closed',
+    'uses' => 'TicketsController@closed'
+]);
+
+Route::get('/solicitud/{id}', [
+    'as' => 'tickets.details',
+    'uses' => 'TicketsController@details'
+]);
 
 Route::controllers([
     'auth' => 'Auth\AuthController',
